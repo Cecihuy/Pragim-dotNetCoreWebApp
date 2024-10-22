07d2b19f83d97408edd9311bc62e2a9360517160
Cecihuy <cecihuy@gmail.com>
2024-10-22 11:21:32 +0700

    12 Static files in asp net core

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -5,7 +5,16 @@ namespace EmployeeManagement {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
-            app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/", () => "Hello World!");
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("home.html");
+            //app.UseDefaultFiles(defaultFilesOptions);
+            //app.UseStaticFiles();
+            FileServerOptions fileServerOptions = new FileServerOptions();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("home.html");
+            app.UseFileServer(fileServerOptions);
             app.Run();
         }
     }
